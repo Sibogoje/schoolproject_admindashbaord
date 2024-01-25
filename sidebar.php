9c3dd9bc746ab0d1e8d3bcc09437815702093e51
Siboniso <sibandzesiboniso0@gmail.com>
2024-01-26 02:46:47 +0530

    enter

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
       <div>
         <div class="brand-logo d-flex align-items-center justify-content-between">
           <a href="index.php" class="text-nowrap logo-img">
-            <img src="logo.png" width="180" alt="" />
+            <img src="logo.png" width="100" alt="" />
           </a>
           <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
             <i class="ti ti-x fs-8"></i>
