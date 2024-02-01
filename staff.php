f4f3bb4b4b714bd816886787b0021ea691ac59b8
Siboniso <sibandzesiboniso0@gmail.com>
2024-02-01 23:51:14 +0530

    Remove unnecessary JavaScript libraries

--- staff.php
+++ staff.php
@@ -155,8 +155,7 @@ function addStaffMember() {
 
   <script src="assets/libs/jquery/dist/jquery.min.js"></script>
   <script src="assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
+
 
   <script src="assets/js/sidebarmenu.js"></script>
   <script src="assets/js/app.min.js"></script>
