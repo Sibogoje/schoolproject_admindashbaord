3d2386fab28d206752ecc5491a327489955e1ddc
Siboniso <sibandzesiboniso0@gmail.com>
2024-02-01 20:06:05 +0530

    Add jQuery library to auth2.php

--- auth2.php
+++ auth2.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Register</title>
   <link rel="shortcut icon" type="image/png" href="logo.png" />
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/styles.min.css" />
 </head>
@@ -77,7 +78,7 @@
       </div>
     </div>
   </div>
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
