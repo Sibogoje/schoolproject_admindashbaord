117d71b6817c904ebd3ac1ee678d7e286f222e43
Siboniso <sibandzesiboniso0@gmail.com>
2024-02-09 03:44:31 +0530

    Fix SQL statement in update_Student.php

--- scripts/update_Student.php
+++ scripts/update_Student.php
@@ -16,11 +16,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
     // Prepare the SQL statement
-    $sql = "UPDATE `students` SET `name` = ?, `surname` = ?, `phone`, `email` = ? WHERE `id` = ?";
+    $sql = "UPDATE `students` SET `name` = ?,  student_id` = ?, `surname` = ?, `phone`, `email` = ? WHERE `id` = ?";
 
     if ($stmt = $conn->prepare($sql)) {
         // Bind parameters to the prepared statement
-        $stmt->bind_param("sssi", $editName, $editSurname, $editPhone, $editEmail, $id);
+        $stmt->bind_param("sisssi", $editName, $editStudent_id, $editSurname, $editPhone, $editEmail, $id);
 
         // Attempt to execute the prepared statement
         if ($stmt->execute()) {
