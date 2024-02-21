25346db657322bcbc762aea06359b486f7771192
Siboniso <sibandzesiboniso0@gmail.com>
2024-02-21 11:12:11 +0530

    Add error reporting and session start to courses.php

--- courses.php
+++ courses.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 require_once 'config.php';
 // Check if the user is logged in, using a session variable (e.g., $_SESSION['loggedin'])
